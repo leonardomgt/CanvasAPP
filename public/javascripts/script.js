1c6d6b679d493fcf7f1b40537d9d5e29a8cd2cbb
Leonardo Teixeira <leogt-15@hotmail.com>
2019-05-29 03:59:14 +0100

    add fabricjs

---
+++ public/javascripts/script.js
@@ -0,0 +1,8 @@
+
+
+var canvas = new fabric.Canvas("canvasapp", { width: 1086, height: 600 });
+// canvas.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
+
+function addShape() {
+	canvas.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
+}
